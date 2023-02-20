f07dc52a05f39b2819365c8cfe672ff865754ade
Om Sharma <omsharma050322@gmail.com>
2023-02-20 22:59:36 +0530

    Initial Commit

---
+++ game.js
@@ -0,0 +1,14 @@
+var candies = ['c1', 'c3', 'c3', 'c4', 'c5'];
+
+var gameBoard = document.getElementsByClassName('game-board')[0];
+var candies = ['c1', 'c3', 'c3', 'c4', 'c5'];
+const NUM_OF_CANDIES = 6;
+const GRID_SIZE = 9;
+
+var swapBox1, swapBox2;
+
+function scanboard() {
+    var arrayOfClasses = [];
+    var boxes = document.getElementsByClassName('box');
+
+}
